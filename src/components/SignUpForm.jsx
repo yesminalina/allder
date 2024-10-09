290b72537b32a912657dac118da5ed6adf79a44e
Yesmin Pizarro <yesmin.pizarro@gmail.com>
2024-10-08 23:36:38 -0300

    Fix name and last name inputs

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -18,11 +18,18 @@ const SignUpForm = () => {
           placeholder='Confirma tu contraseña'
           className='mt-1 mb-4 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
         />
-        <input
-          type='text'
-          placeholder='Nombre de usuario'
-          className='mt-1 mb-4 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
-        />
+        <article className='flex flex-row gap-3'>
+          <input
+            type='text'
+            placeholder='Nombre'
+            className='mt-1 mb-4 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
+          />
+          <input
+            type='text'
+            placeholder='Apellido'
+            className='mt-1 mb-4 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500'
+          />
+        </article>
         <button className='p-3 mb-4 bg-green-400 rounded-lg'>Crea tu cuenta</button>
       </div>
       <div className='py-6 bg-slate-200 text-center'>
